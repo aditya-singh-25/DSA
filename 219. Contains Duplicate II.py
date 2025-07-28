71208af5c1447603b0699e0a26862daa5823821c
ADITYA SINGH <aditya2502singh@gmail.com>
2025-07-29 00:18:47 +0530

    contains duplicate || dsa problem

---
+++ 219. Contains Duplicate II.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        seen = set()
+        for i,val in enumerate(nums):
+            if val in seen:
+                return True
+            seen.add(val)
+            if len(seen)>k:
+                seen.remove(nums[i-k])
+        return False
