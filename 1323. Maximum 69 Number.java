3c3aa71a5d48c2924bc1307259720b2cb13dcc05
ADITYA SINGH <aditya2502singh@gmail.com>
2025-08-16 18:07:10 +0530

    max 69 number - java

---
+++ 1323. Maximum 69 Number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximum69Number (int num) {
+        
+        String numStr = String.valueOf(num);
+        char[] digits = numStr.toCharArray();
+
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] == '6') {
+                digits[i] = '9';
+                break;  
+            }
+        }
+        return Integer.parseInt(new String(digits));
+    }
+}
